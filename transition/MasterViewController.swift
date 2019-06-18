bb52be69f99d522df8952e46b41bce190187c9da
kochi-rin <kochi.rin@zozo.com>
2019-06-18 12:31:29 +0900

    add navigation animation

---
+++ transition/MasterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MasterViewController.swift
+//  transition
+//
+//  Created by kouchi.rin on 2019/06/18.
+//  Copyright © 2019 kouchi.rin. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ transition/NavigationController.swift
@@ -0,0 +1,48 @@
+//
+//  NavigationController.swift
+//  transition
+//
+//  Created by kouchi.rin on 2019/06/18.
+//  Copyright © 2019 kouchi.rin. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        delegate = self
+    }
+
+    // Push: From -> To / Pop: From <- To それぞれのペアを定義する
+    private var classPairs: [(from: AnyClass, to: AnyClass)] {
+        return [
+            (from: MasterViewController.self, to: DetailViewController.self),
+            (from: DetailViewController.self, to: MasterViewController.self),
+        ]
+    }
+
+    private func isTargetViewControllerPair(from: UIViewController, to: UIViewController) -> Bool {
+        for pair in classPairs {
+            if from.classForCoder == pair.from && to.classForCoder == pair.to {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+extension NavigationController: UINavigationControllerDelegate {
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if isTargetViewControllerPair(from: fromVC, to: toVC) {
+            return SheetWithHeaderAnimator(operation)
+        }
+
+        return nil
+    }
+}
